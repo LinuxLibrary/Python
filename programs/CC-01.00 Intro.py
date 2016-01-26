29acb477901da233c77a449b24fda515241baf98
Arjun Shrinivas <vmsnivas@gmail.com>
2016-01-26 23:01:40 +0000

    Added directories

--- programs/CC-01.00 Intro.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-monty = True
-python = 1.234
-monty_python = python ** 2
-
-print monty, python, monty_python
-print "What you are seeing is %s, you are using Python %s, Monty has Re. %s with him." %(monty,python,monty_python)
