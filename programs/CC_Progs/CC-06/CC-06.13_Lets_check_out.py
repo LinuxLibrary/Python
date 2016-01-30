3a3b0a1eba7aaa79dca626bbcbe5b468b2f5721c
Arjun Shrinivas <vmsnivas@gmail.com>
2016-01-30 22:53:12 +0000

    Added all the programs of CC-06

---
+++ programs/CC_Progs/CC-06/CC-06.13_Lets_check_out.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+shopping_list = ["banana", "orange", "apple"]
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+    total = 0
+    for x in food:
+        if stock[x] > 0:
+            total += prices[x]
+            stock[x] -= 1
+    return total
+
+#print compute_bill(shopping_list)
+print stock
