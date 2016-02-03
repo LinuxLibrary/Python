d1e047d1a7e445456e64a14da6baeb6825477f49
Arjun Shrinivas <vmsnivas@gmail.com>
2016-02-03 11:21:22 +0000

    Modified CC-08.0.08

--- programs/CC_Progs/CC-08/CC-08.0/CC-08.0.08_You_own_while_else.py
+++ programs/CC_Progs/CC-08/CC-08.0/CC-08.0.08_You_own_while_else.py
@@ -7,10 +7,14 @@ guesses_left = 3
 # Start your game!
 while guesses_left <=3:
     random_number = randint(1,10)
+    print random_number
     guess = int(raw_input("Please input your guess : "))
     if guess == random_number :
         print "You win!"
         break
         guesses_left -=1
-else : 
+#    else:
+#        print "Try again _/\_"
+#        break
+else: 
     print "You lose!"
