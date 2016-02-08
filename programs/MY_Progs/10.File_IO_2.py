c06e69e23d4cb7a5b62ba468a4818c0af5aed88d
Arjun Shrinivas <vmsnivas@gmail.com>
2016-02-08 07:37:31 +0000

    Modified 10.File_IO_2.py

--- programs/MY_Progs/10.File_IO_2.py
+++ programs/MY_Progs/10.File_IO_2.py
@@ -3,11 +3,15 @@
 # Date : 08.02.2016
 # Purpose: Illustrates Python's File I/O capabilities
 
-han1 = open(f1,"w")
+han1 = open("f1.txt","w")
 blogname = "Linux-Library"
 blogurl = "http://linux-library.blogspot.in"
 count = 1
-han1.write(blogname,blogurl)
-han1.close()
 
+# % operator when applied to strings performs formatting
+# %s - strings, %d - integers, %f - floats
+while count <= 10:
+	han1.write("%d\t%s\t%s\n" % (count,blogname,blogurl))
+	count += 1
+han1.close()
 # END
