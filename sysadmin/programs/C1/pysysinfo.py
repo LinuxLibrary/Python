5a054e772aa12ca6f622ea8734d33398885656b3
root <vmsnivas@gmail.com>
2016-04-23 08:38:48 +0000

    C1 completed upto : Reusing code with the import statement P17

---
+++ sysadmin/programs/C1/pysysinfo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def uname_func():
+    uname = "uname"
+    uname_arg = "-a"
+    print "Gathering system information with %s command :\n" % uname
+    subprocess.call([uname, uname_arg])
+
+def disk_func():
+    diskspace = "df"
+    diskspace_arg = "-hT"
+    print "Gathering diskspace information using %s command:\n" % diskspace
+    subprocess.call([diskspace, diskspace_arg])
+
+def main():
+	uname_func()
+	disk_func()
+
+main()
