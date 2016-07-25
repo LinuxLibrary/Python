36809fbc6b938d74f2e369e63e8feb62895a34f1
Arjun Shrinivas <vmsnivas@gmail.com>
2016-07-25 04:00:45 +0000

    Re-organized files in programs directories PY4INF

---
+++ py4inf/1-pe-py/programs/11-asg1-test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+name = raw_input("Enter your name")
+print "Hello", name
--- py4inf/1-pe-py/programs/21-asg1-test.py
+++ py4inf/1-pe-py/programs/21-asg1-test.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
 
-name = raw_input("Enter your name")
-print "Hello", name
+fh = open("mbox-short.txt", "r")
+
+count = 0
+for line in fh:
+	if not line.startswith('From '): continue
+	print line.strip()
+	count = count + 1
+        
+print count, "MBOX-Short Lines: "
