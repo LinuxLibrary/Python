45f7dd7a67b587f1aad1c1cb7d1c104ef0969465
Arjun Shrinivas <vmsnivas@gmail.com>
2016-08-22 18:19:34 +0000

    Added 71-chgmgmt1.py

---
+++ py4inf/3-wd-py/programs/71-chgmgmt1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import requests, json
+url = 'https://spscommerce.atlassian.net/rest/api/2/search?jql=project=CHGMGMT&fields=customfield_16002&maxResults=10'
+username = 'displaymonitor'
+password = 'CDZha6u8'
+
+data = requests.get(url, auth=(username, password)).content
+
+envcount = dict()
+
+jdat = json.loads(str(data))
+jsdata = jdat['issues']
+
+for line in jsdata:
+	env = line['fields']["customfield_16002"]["value"]
+	envcount[env] = envcount.get(env, 0) + 1
+
+casebyenv = json.dumps(envcount)
+print casebyenv
