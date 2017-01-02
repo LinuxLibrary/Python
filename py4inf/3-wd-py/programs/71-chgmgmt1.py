ae6db4adeffc53695c329160a21a3ec387a292c8
Arjun Shrinivas <vmsnivas@gmail.com>
2017-01-02 13:12:35 -0600

    Modified 71-chgmgmt1.py

--- py4inf/3-wd-py/programs/71-chgmgmt1.py
+++ py4inf/3-wd-py/programs/71-chgmgmt1.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python
 
 import requests, json
-<<<<<<< HEAD
-url = 'https://XXXXXXXXXXX.atlassian.net/rest/api/2/search?jql=project=CHGMGMT&fields=customfield_16002&maxResults=87'
-=======
-<<<<<<< HEAD
-url = 'https://spscommerce.atlassian.net/rest/api/2/search?jql=project=CHGMGMT&fields=customfield_16002&maxResults=10'
+url = 'https://XXXXXXXXXXX.atlassian.net/rest/api/2/search?jql=project=CHNG&fields=customfield_16002&maxResults=87'
 username = 'XXXXXXXXX'
 password = 'xxxxxxxxx'
-=======
-url = 'https://spscommerce.atlassian.net/rest/api/2/search?jql=project=CHGMGMT&fields=customfield_16002&maxResults=87'
->>>>>>> 18d30248021d7702bd70db7f34f6f250db68655f
-username = 'XXXXXXXX'
-password = 'xxxxxxxx'
->>>>>>> c19022dbe3b901fc814b9c36d2844970366af054
 
 data = requests.get(url, auth=(username, password)).content
 
