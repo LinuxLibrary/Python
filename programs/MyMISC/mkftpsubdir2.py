5947e7d4e312b739564e0007625060ac34d28f00
Arjun Shrinivas <vmsnivas@gmail.com>
2017-04-26 02:41:44 +0000

    Added mkftpsubdir2.py

---
+++ programs/MyMISC/mkftpsubdir2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Author : Arjun Shrinivas
+# Date : 25.04.2017
+
+
+# This script is used to create subdirectories within ftp customer home directories
+
+######################
+## Revision History ##
+######################
+20170425  AS  Created
+
