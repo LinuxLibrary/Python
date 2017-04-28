f00a3285b13a1317c34da1da844b1af5afb5e2af
Arjun Shrinivas <vmsnivas@gmail.com>
2017-04-28 05:15:07 +0000

    Removed programs/CC_Progs/CC-05/CC-05.0/CC-05.0.06_Maintaining_order.py

--- programs/CC_Progs/CC-05/CC-05.0/CC-05.0.06_Maintaining_order.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
-duck_index = animals.index("duck")   # Use index() to find "duck"
-
-# Your code here!
-animals = animals.insert(3, "cobra")
-
-print animals # Observe what prints after the insert operation
