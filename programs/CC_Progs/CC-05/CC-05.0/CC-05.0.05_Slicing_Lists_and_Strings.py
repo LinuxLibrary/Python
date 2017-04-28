bed4ea2d1cb3e90d8b2c2383952c2e60890a0ce4
Arjun Shrinivas <vmsnivas@gmail.com>
2017-04-28 05:15:07 +0000

    Removed programs/CC_Progs/CC-05/CC-05.0/CC-05.0.05_Slicing_Lists_and_Strings.py

--- programs/CC_Progs/CC-05/CC-05.0/CC-05.0.05_Slicing_Lists_and_Strings.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-animals = "catdogfrog"
-cat  = animals[0:3]   # The first three characters of animals
-dog  = animals[3:6]  # The fourth through sixth characters
-frog = animals[6:10]  # From the seventh character to the end
