3133518811b2ed79f58913ea6032fafbc28cdea2
Arjun Shrinivas <vmsnivas@gmail.com>
2017-04-28 05:15:08 +0000

    Removed programs/CC_Progs/CC-08/CC-08.0/CC-08.0.15_Counting_as_you_go.py

--- programs/CC_Progs/CC-08/CC-08.0/CC-08.0.15_Counting_as_you_go.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-choices = ['pizza', 'pasta', 'salad', 'nachos']
-print 'Your choices are:'
-for index, item in enumerate(choices):
-    #print index, item
-    print index+1, item
