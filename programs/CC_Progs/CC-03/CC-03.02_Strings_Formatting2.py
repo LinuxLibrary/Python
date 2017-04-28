4581ed1070cfba5086cfd81a22a67200d3b491dc
Arjun Shrinivas <vmsnivas@gmail.com>
2017-04-28 05:15:06 +0000

    Removed programs/CC_Progs/CC-03/CC-03.02_Strings_Formatting2.py

--- programs/CC_Progs/CC-03/CC-03.02_Strings_Formatting2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-name = raw_input("What is your name?")
-quest = raw_input("What is your quest?")
-color = raw_input("What is your favorite color?")
-
-print "Ah, so your name is %s, your quest is %s, " \
-"and your favorite color is %s." % (name, quest, color)
