60136750895cfc2c644d27dd407a0921992ac586
Arjun Shrinivas <vmsnivas@gmail.com>
2017-04-28 05:15:08 +0000

    Removed programs/CC_Progs/CC-08/CC-08.0/CC-08.0.19_Create_your_own.py

--- programs/CC_Progs/CC-08/CC-08.0/CC-08.0.19_Create_your_own.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-for i in range(2):
-    choice = raw_input("Please input your name : ")
-    if choice != "Arjun":
-        print "Entry restricted!"
-        break
-else:
-    print "Welcome Geek!"
