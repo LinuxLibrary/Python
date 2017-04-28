7bb3a8d9205055202a6b8467ed2c408875c9dee9
Arjun Shrinivas <vmsnivas@gmail.com>
2017-04-28 05:15:08 +0000

    Removed programs/CC_Progs/CC-08/CC-08.0/CC-08.0.03_While_you_are_at_it.py

--- programs/CC_Progs/CC-08/CC-08.0/CC-08.0.03_While_you_are_at_it.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-num = 1
-while num <= 10:  # Fill in the condition
-    print "Num is %s and its sqare is %s" % (num, num ** 2)# Print num squared
-    print num ** 2
-    num += 1# Increment num (make sure to do this!)
