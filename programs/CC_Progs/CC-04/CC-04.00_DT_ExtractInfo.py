c0a6bfa350b3dc8b37e4940008d798ee729683cd
Arjun Shrinivas <vmsnivas@gmail.com>
2017-04-28 05:15:07 +0000

    Removed programs/CC_Progs/CC-04/CC-04.00_DT_ExtractInfo.py

--- programs/CC_Progs/CC-04/CC-04.00_DT_ExtractInfo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from datetime import datetime
-
-now = datetime.now()
-print "Current year is\t\t: ", now.year,"\n", "Current month is\t: ", now.month, "\n", "Today's date is\t\t: ", now.day
-print now
