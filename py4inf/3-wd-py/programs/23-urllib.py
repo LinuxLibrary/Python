d05b4ca8bf1e930bd83cd3bff5c87075d91946a4
Arjun Shrinivas <vmsnivas@gmail.com>
2017-04-28 05:12:06 +0000

    Removed py4inf/3-wd-py/programs/23-urllib.py

--- py4inf/3-wd-py/programs/23-urllib.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import urllib
-#fh = urllib.urlopen('http://www.py4inf.com/code/romeo.txt')
-fh = urllib.urlopen('http://www.linux-library.in')
-
-for line in fh:
-	print line.strip()
