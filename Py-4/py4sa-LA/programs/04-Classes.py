03d98fa570ea451c15f922a3fd6e92b99538a26c
Arjun Shrinivas <vmsnivas@gmail.com>
2017-05-03 07:00:13 +0000

    Modified 04-Classes.py

--- Py-4/py4sa-LA/programs/04-Classes.py
+++ Py-4/py4sa-LA/programs/04-Classes.py
@@ -3,6 +3,23 @@
 # Date : 03.05.2017
 # Purpose : Classes in Python
 
+class Car():
+	def __init__(self):
+		print "Car started"
+	def color(self,color)
+		print "Your %s car is looking awesome" % color
+	def accel(self,speed):
+		print "Speeding upto %s mph" % speed
+	def turn(self,direction):
+		print "Turning " + direction
+	def stop(self):
+		print "Stop"
+car1 = Car()
+print car1
+print car1.color('Black Chevrolet Cobalt SS')
+print car1.accel(50)
+print car1.turn('Right')
+print car1.stop()
 
 
 # END
