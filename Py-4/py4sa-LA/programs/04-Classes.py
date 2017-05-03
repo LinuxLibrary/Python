b4c8f6cfaa051ed4e2c8cbfaf059a36bf95449cb
Arjun Shrinivas <vmsnivas@gmail.com>
2017-05-03 07:11:10 +0000

    Modified 04-Classes.py

--- Py-4/py4sa-LA/programs/04-Classes.py
+++ Py-4/py4sa-LA/programs/04-Classes.py
@@ -21,5 +21,16 @@ print car1.accel(50)
 print car1.turn('Right')
 print car1.stop()
 
+class RaceCar(Car):
+	def __init__(self,color):
+		self.color = color
+		self.top_speed = 200
+		print "%s race car has started with a top speed of %s" % (self.color, self.top_speed)
+	def accel(self, speed):
+		print "%s speeding up to %s mph very very fast" % (self.color, speed)
+
+car2 = RaceCar('Black Chevrolet Cobalt SS')
+print car2
+print car2.accel(80)
 
 # END
