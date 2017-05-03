14586e400aa491fccd1d35c3b668146556facf07
Arjun Shrinivas <vmsnivas@gmail.com>
2017-05-03 07:42:01 +0000

    Added 06-Generators.py

---
+++ Py-4/py4sa-LA/programs/06-Generators.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# Author : Arjun Shrinivas
+# Date : 03.05.2017
+# Purpose : Using Generators in Python
+
+def counter():
+	i = 0
+	while True:
+		return i
+a = counter()
+print type(a)
+print a
+print a
+print "There is no change. Let us try using Generators"
+
+def counter():
+	i = 0
+	while True:
+		yield i
+a = counter()
+print type(a)
+print a
+print a
+print a
+
+# END
