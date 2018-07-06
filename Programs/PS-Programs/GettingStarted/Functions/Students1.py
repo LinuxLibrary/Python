17b8b92d93a29878c96fc66dd1e886effff615e4
Arjun Shrinivas <vmsnivas@gmail.com>
2018-07-06 19:25:02 +0530

    Added Programs/PS-Programs/GettingStarted/Functions/Students1.py

---
+++ Programs/PS-Programs/GettingStarted/Functions/Students1.py
@@ -0,0 +1,16 @@
+students = []
+
+def get_students_titlecase():
+	students_titlecase = []
+	for student in students:
+		students_titlecase =student.title()
+	return students_titlecase
+	
+def print_students_titlecase:
+	students_titlecase = []
+	for student in students:
+		students_titlecase =student.title()
+	print(students_titlecase)
+	
+def add_student(name):
+	students.append(name)
