c443e1f64935e1cbe729e8b40042333f83ad8f3e
Arjun Shrinivas <vmsnivas@gmail.com>
2018-07-13 19:23:01 +0530

    Modified Programs/PS-Programs/GettingStarted/Functions/Students1.py

--- Programs/PS-Programs/GettingStarted/Functions/Students1.py
+++ Programs/PS-Programs/GettingStarted/Functions/Students1.py
@@ -6,11 +6,13 @@ def get_students_titlecase():
 		students_titlecase =student.title()
 	return students_titlecase
 	
-def print_students_titlecase:
-	students_titlecase = []
-	for student in students:
-		students_titlecase =student.title()
+def print_students_titlecase():
+	students_titlecase = get_students_titlecase()
 	print(students_titlecase)
 	
 def add_student(name):
 	students.append(name)
+	
+student_list = get_students_titlecase()
+
+add_student("Mark")
