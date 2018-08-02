c85bf6f1bd109d8211da722f43bf2a7d53e40c09
Arjun Shrinivas <vmsnivas@gmail.com>
2018-08-02 18:34:58 +0530

    Modified Programs/PS-Programs/GettingStarted/04-Classes/Classes1.py

--- Programs/PS-Programs/GettingStarted/04-Classes/Classes1.py
+++ Programs/PS-Programs/GettingStarted/04-Classes/Classes1.py
@@ -1,13 +1,20 @@
 students = []
 
 class Student:
+
+	school_name = "My School"
+	
 	def __init__(self, name, student_id=332):
 		student = {"name": name, "student_id": student_id}
 		students.append(student)
 	
 	def __str__(self):
-		return "Student"
+		return "Student " + self.name
+		
+	def get_name_capitalize(self):
+		return self.name.capitalize()
 		
 		
 mark = Student("Mark")
-print(students)
+print(mark)
+print(Student.school_name)
