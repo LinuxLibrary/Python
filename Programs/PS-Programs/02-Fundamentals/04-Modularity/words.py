5b37a714847802623503492cd97c8b521a2d06a9
Arjun Shrinivas <vmsnivas@gmail.com>
2018-08-20 20:03:03 +0530

    Modified Programs/PS-Programs/02-Fundamentals/04-Modularity/words.py

--- Programs/PS-Programs/02-Fundamentals/04-Modularity/words.py
+++ Programs/PS-Programs/02-Fundamentals/04-Modularity/words.py
@@ -1,7 +1,22 @@
+"""Retrieve and print words from a URL.
+
+Usage:
+	python3 words.py <URL>
+"""
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-	with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+	"""Fetch a list of words from a URL.
+	
+	Args:
+		url: The URL of a UTF-8 text document.
+		
+	Returns:
+		A list of strings containing the words from
+		the document.
+	"""
+	with urlopen(url) as story:
 		story_words = []
 		for line in story:
 			line_words = line.decode('utf-8').split()
@@ -10,15 +25,25 @@ def fetch_words():
 	return story_words
 	
 
-def print_words(story_words):
-	for word in story_words:
-		print(word)
+def print_words(items):
+	"""Print items one per line.
+	
+	Args:
+		An iterable series of printable items.
+	"""
+	for item in items:
+		print(item)
 		
 
-def main():
+def main(url):
+	"""Print each word from a text document from a URL.
+	
+	Args:
+		url: The URL of a UTF-8 text document.
+	"""
 	words = fetch_words()
 	print_words(words)
 	
 	
 if __name__ = '__main__':
-	main()
+	main(sys.argv[1])
