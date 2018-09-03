948747739ba3aa787d59c0b233e66ad5d97cf8d7
Arjun Shrinivas <vmsnivas@gmail.com>
2018-09-03 19:29:43 +0530

    Modified Programs/PS-Programs/02-Fundamentals/07-ExceptionHandling/02-Exceptions.py

--- Programs/PS-Programs/02-Fundamentals/07-ExceptionHandling/02-Exceptions.py
+++ Programs/PS-Programs/02-Fundamentals/07-ExceptionHandling/02-Exceptions.py
@@ -2,12 +2,15 @@
 
 '''A module for demonstrating exceptions.'''
 
+import sys
+
 def convert(s):
 	'''Convert to an integer.'''
 	try:
 		x = int(s)
 		print("Convertion succeeded! x =", x)
-	except (ValueError, TypeError):
-		print("Conversion failed!")
-		x = -1
-	return x
+	except (ValueError, TypeError) as e:
+		print("Conversion error: {}"\
+				.format(str(e)),
+				file=sys.stderr)
+		raise # Re-raise exception
