c234c0276f45bd6303a8dd55bc33b310697f65b9
Arjun Shrinivas <vmsnivas@gmail.com>
2018-09-04 19:02:57 +0530

    Added Programs/PS-Programs/02-Fundamentals/08-Comprehensions/01-FilteringPredicates.py

---
+++ Programs/PS-Programs/02-Fundamentals/08-Comprehensions/01-FilteringPredicates.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def is_prime(x):
+	if x < 2:
+		return False
+	for i in range(2, int(sqrt(x)) + 1):
+		if x % i == 0:
+			return False
+	return True
+	
+[x for x in range(101) if is_prime(x)]
