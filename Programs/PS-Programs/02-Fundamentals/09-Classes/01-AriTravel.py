ec2813de087eea626ab04a43500a30914d26adb0
Arjun Shrinivas <vmsnivas@gmail.com>
2018-09-06 14:50:35 +0530

    Modified Programs/PS-Programs/02-Fundamentals/09-Classes/01-AriTravel.py

--- Programs/PS-Programs/02-Fundamentals/09-Classes/01-AriTravel.py
+++ Programs/PS-Programs/02-Fundamentals/09-Classes/01-AriTravel.py
@@ -3,8 +3,9 @@
 """Model for aircraft flights"""
 
 class Flight:
-
-	def __init__(self, number):
+	"""A flight with a particular passenger aircraft."""
+	
+	def __init__(self, number, aircraft):
 		if not number[:2].isalpha():
 			raise ValueError("No airline code in '{}'".format(number))
 		
@@ -15,6 +16,44 @@ class Flight:
 			raise ValueError("Invalid route number '{}'".format(number))
 			
 		self._number = number
+		self._aircraft = aircraft
 		
 	def number(self):
 		return self._number
+		
+	def airline(self):
+		return self._number[:2]
+		
+	def aircraft_model(self):
+		return self._aircraft.model()
+		
+		
+class Aircraft:
+
+	def __init__(self, registration, model, num_rows, num_seats_per_row):
+		self._registration = registration
+		self._model = model
+		self._num_rows = num_rows
+		self._num_seats_per_row = num_seats_per_row
+		
+	def registration(self):
+		return self._registration
+		
+	def model(self):
+		return self._model
+		
+	def seating_plan(self):
+		return (range(1, self._num_rows + 1), "ABCDEFGHJK"[:self._num_seats_per_row])
+		
+
+'''
+Sample execution:
+from 01-AirTravel import *
+
+a = Aircraft("G-EUPT", "Airbus A139", num_rows=22, num_seats_per_row=6)
+a.registration()
+a.model()
+a.seating_plan()
+
+f = Flight("BA758", Aircraft("G-EUPT", "Airbus A139", num_rows=22, num_seats_per_row=6))
+'''
